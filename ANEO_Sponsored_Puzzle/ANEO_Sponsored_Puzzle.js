d58713e2a1bda36fb489f732c52d34abe405c280
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-01-26 19:46:38 +0100

    Ajout ANEO_Sponsored_Puzzle js

---
+++ ANEO_Sponsored_Puzzle/ANEO_Sponsored_Puzzle.js
@@ -0,0 +1,30 @@
+const speed = parseInt(readline());
+const lightCount = parseInt(readline());
+let tabDuration = [];
+let tabDistance = [];
+for (let i = 0; i < lightCount; i++) {
+    var inputs = readline().split(' ');
+    const distance = parseInt(inputs[0]);
+    const duration = parseInt(inputs[1]);
+    tabDuration[i] = duration;
+    tabDistance[i] = distance;
+}
+
+let speedFinal;
+let newSpeed = speed;
+let compteur;
+while (lightCount != compteur) {
+    compteur = 0;
+    for (let k=0;k<tabDuration.length;k++) {
+        if (Math.floor(newSpeed*tabDuration[k]/3.6) > tabDistance[k] || 
+            Math.floor(newSpeed*tabDuration[k]/3.6)%tabDistance[k] === 0 || 
+            Math.floor( tabDistance[k]/ (newSpeed*tabDuration[k]/3.6) )%2 === 0 || 
+            Math.floor(newSpeed*tabDuration[k]/3.6) === Math.floor(tabDistance[k]%(newSpeed*tabDuration[k]/3.6))) {
+                compteur++;
+        }
+        if (lightCount === compteur) {
+            console.log(newSpeed);
+        }
+    }
+    newSpeed--;
+}
