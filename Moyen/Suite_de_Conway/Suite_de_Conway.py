5845bdf81119f9947cb15c76c2c0c931827ed018
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-07-06 22:45:39 +0200

    ajout Suite de Conway py

---
+++ Moyen/Suite_de_Conway/Suite_de_Conway.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+array = [int(input())]
+l = int(input())
+for i in range(1, l):
+    new_array = []
+    num, last = 0, -1
+    for v in array:
+        if v != last:
+            if num != 0:
+                new_array.append(num)
+                new_array.append(last)
+            num = 1
+        else:
+            num +=1
+        last = v
+    new_array.append(num)
+    new_array.append(last)
+    array = new_array
+print(' '.join(str(v) for v in array))
